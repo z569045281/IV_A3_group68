d8af96c20902bc822d86721e950359faffab3c1d
Tianchen Zheng <tianczheng@student.unimelb.edu.au>
2021-10-16 17:51:19 +1100

    optimize landing page

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const menuToggle = document.querySelector('.toggle');
+const menuToggleButton = document.querySelector('.toggle1');
 const showcase = document.querySelector('.showcase');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
 })
+
+menuToggleButton.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
