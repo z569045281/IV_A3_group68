d65ba30f79d29f11a6085a8bc6143cf3d03bb05d
Tianchen Zheng <tianczheng@student.unimelb.edu.au>
2021-10-21 18:46:36 +1100

    lin work

---
+++ layout_2/script.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector('.toggle');
+const menuToggleButton = document.querySelector('.toggle1');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
+
+menuToggleButton.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
