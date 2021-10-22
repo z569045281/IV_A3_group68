02870a031d427fd3b804f42119dbb41318442857
Tianchen Zheng <tianczheng@student.unimelb.edu.au>
2021-10-22 19:38:16 +1100

    side bar change

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const menuToggle = document.querySelector('.toggle');
 const menuToggleButton = document.querySelector('.toggle1');
 const showcase = document.querySelector('.showcase');
 
+
+//side menu  
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
