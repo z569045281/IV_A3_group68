2465289aeb365e616a746f914765df6c4fa6b58d
s3554374 <s3554374>
2021-10-23 23:34:56 +1100

    test

--- layout_3/config.js
+++ layout_3/config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/booker619/ckv241tj62zk015qocnjjxvxx/draft',
+    style: 'mapbox://styles/booker619/ckv241tj62zk015qocnjjxvxx',
     accessToken: 'pk.eyJ1IjoiYm9va2VyNjE5IiwiYSI6ImNrdG8xandteTA3ZXUzMG1scDM1Z3RzdDUifQ.DKp5ItneV3gjnQOXpMfaVA',
     showMarkers: false,
     markerColor: '#3FB1CE',
@@ -8,15 +8,15 @@ var config = {
     title: '',
     subtitle: '',
     byline: '',
-    footer: 'Source: source citations, etc.',
+    footer: '<h2><a href="../index.html">Back to Index</a></h2>',
     chapters: [
         {
             id: 'index',
             alignment: 'right',
             hidden: false,
-            title: 'Display Title',
+            title: 'Welcome to Melbourne!',
             image: '',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: '',
             location: {
                 center: [144.97075, -37.82982],
                 zoom: 12,
@@ -61,9 +61,9 @@ var config = {
             id: 'CBD',
             alignment: 'left',
             hidden: false,
-            title: 'Second Title',
+            title: 'Coffe and Restaurants',
             image: '',
-            description: 'Copy these sections to add to your story.',
+            description: 'Melbourne is a city that lives and breathes coffee – what started with the city \'s European migrant wave has now developed into a specialised field. And brunch – whether its a green matcha fuelled adventure, or a classic stack of pancakes – is almost a competitive sport in a city with AM dining of this calibre. We scoured Melbourne to bring you a guide to the best of the best.',
             location: {
                 center: [144.96305, -37.81433],
                 zoom: 15.52,
@@ -134,9 +134,9 @@ var config = {
             id: 'top5',
             alignment: 'full',
             hidden: false,
-            title: 'test',
+            title: 'Five Cafes and restaurants you need to visit',
             image: '',
-            description: 'Copy these sections to add to your story.',
+            description: 'Melbourne is a city that is renowned for its fantastic coffee and amazing cafe culture. From hidden CBD gems and suburban roasteries to the chic and stylish cafes that line Chapel Street, Melbourne provides a wealth of incredible locations where you can sip the finest brew and consume the most delicious dishes.',
             location: {},
             mapAnimation: '',
             rotateAnimation: false,
@@ -144,39 +144,15 @@ var config = {
             onChapterEnter: [],
             onChapterExit: []
         },
-        
-        {
-            id: 'lunaCBD',
-            alignment: 'left',
-            hidden: false,
-            title: 'Luna CBD',
-            image: 'https://lh3.googleusercontent.com/proxy/uUcNo-IlhjVxR39Sgnx7xNP1KXJBVMIsnBB8mOb4NCaaw1hWbTy_aDy_S_9lyqczm2jr76g6TmYSapBQUk_EErgzcMYUudOd4w_9T26eDNmrgjxW4LfobMEsrrCbX78pFoPYF5mx',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [144.96942, -37.81551],
-                zoom: 18.91,
-                pitch: 46.50,
-                bearing: -73.20
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-                {
-                layer: 'luna',
-                opacity: 1
-                }
-            ],
-            onChapterExit: []
-        },
 
         {
             id: 'luna',
             alignment: 'right',
-            hidden: true,
-            title: 'Luna',
+            hidden: false,
+            title: 'Lune Croissanterie',
             image: 'https://travelintwos.com/wp-content/uploads/2019/08/photo0jpg.jpg',
-            description: 'Copy these sections to add to your story.',
+            description: 'Melbourne’s Lune Croissanterie has been named home of the World’s Best Croissant. Inspired by the delicate pastry of France, the Lune team reconstructed it to create a pastry worth lining up for',
+            address: '119 Rose St, Fitzroy VIC 3065',
             location: {
                 center: [144.9803769880091,-37.79522817823646],
                 zoom: 15.59,
@@ -190,6 +166,32 @@ var config = {
             onChapterExit: []
         },
 
+        //{
+        //    id: 'lunaCBD',
+        //    alignment: 'left',
+        //    hidden: false,
+        //    title: 'Luna CBD',
+        //    image: 'https://lh3.googleusercontent.com/proxy/uUcNo-IlhjVxR39Sgnx7xNP1KXJBVMIsnBB8mOb4NCaaw1hWbTy_aDy_S_9lyqczm2jr76g6TmYSapBQUk_EErgzcMYUudOd4w_9T26eDNmrgjxW4LfobMEsrrCbX78pFoPYF5mx',
+        //    description: ' ',
+        //    address:'Shop 16/161 Collins Street, Russell St, Melbourne VIC 3000',
+        //    location: {
+        //        center: [144.96942, -37.81551],
+        //       zoom: 18.91,
+        //        pitch: 46.50,
+        //       bearing: -73.20
+        //    },
+        //    mapAnimation: 'flyTo',
+        //    rotateAnimation: false,
+        //    callback: '',
+        //    onChapterEnter: [
+        //        {
+        //        layer: 'luna',
+        //        opacity: 1
+        //        }
+        //    ],
+        //   onChapterExit: []
+        //},
+
         {
             id: 'sevenseed',
             alignment: 'left',
@@ -197,7 +199,8 @@ var config = {
             title: 'Sevenseed',
             image: 'http://eatosaurusrex.com/wp-content/uploads/2017/09/20150524_seven-seeds-melbourne_4538.jpg',
             image2: 'https://cdn.broadsheet.com.au/cache/2c/d0/2cd099270c350ee0b17cf51d8148cc74.jpg',
-            description: 'Copy these sections to add to your story.',
+            description: 'Stop in at Carlton for exactingly brewed cup of the Golden Gate espresso blend with notes of cocoa and toffee, or try a rotating single origin made with the method of your choice. The place is regularly busy and always humming with energy. It’s not uncommon to have to wait for a table. Melbourne University is only a block away so students often drop by while they kill time between classes.',
+            address:'114 Berkeley St, Carlton VIC 3053',
             location: {
                 center: [144.95878, -37.80286],
                 zoom: 19.93,
@@ -226,8 +229,9 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Tigerous Espresso',
-            image: 'https://i2.wp.com/www.zestcoffee.com.au/app/uploads/2018/12/IMG-4124.JPG?fit=1000%2C1778&ssl=1',
-            description: 'Copy these sections to add to your story.',
+            image: 'https://i1.wp.com/www.zestcoffee.com.au/app/uploads/2019/08/Zest_Tigerous_0110_9527.jpg?fit=320%2C480&ssl=1',
+            description: 'Tigerous is Caleb’s first café, which he runs with his wife Abby, a certified nutritionist (and Caleb’s latest barista protégé). However new he is to owning a café, it’s definitely not Caleb’s first rodeo. The Tiger’s café heritage spans a decade in Melbourne’s evolving specialty scene. And like so many coffee professionals of his ilk, Caleb cut his teeth as a junior shot puller and worked his way up to head barista and eventually café manager, evolving his (now champion) skillset as the scene evolved.',
+            address:'361 Queen St, Melbourne VIC 3000',
             location: {
                 center: [144.9582124,-37.81022002],
                 zoom: 19,
@@ -262,7 +266,8 @@ var config = {
             title: 'Chin Chin Restaurant',
             image: 'https://media-cdn.tripadvisor.com/media/photo-s/02/26/5d/3b/chin-chin.jpg',
             image2: 'https://img.delicious.com.au/DLtwS8oH/w759-h506-cfill/del/2017/11/chin-chin-melbourne-55365-5.jpg',
-            description: 'Copy these sections to add to your story.',
+            description: 'An australian take on design, art and bold south-east asian flavours, chin chin marches to the beat of its own drum.',
+            address:'125 Flinders Ln, Melbourne VIC 3000',
             location: {
                 center: [144.9703999,-37.81568917],
                 zoom: 18.91,
@@ -283,7 +288,8 @@ var config = {
             title: 'Di Stasio Citta',
             image: "https://img.delicious.com.au/FWk0USFf/w1200/del/2019/10/di-stasio-citta-melbourne-117125-2.jpg",
             image2: 'https://www.indeawards.com/wp-content/uploads/2020/05/The-Social-Space_Di-Stasio-Citta_HASSELL_3_Ph-Peter-Bennetts_1500x1000.jpg',
-            description: 'Copy these sections to add to your story.',
+            description: 'A Milan end for Spring St, an Italian quarter for the Melbourne CBD: a diverse range of human activities; a way of expression that combines music, literature, film, painting, photography, design and shared celebration, an electric sense of anticipation in the air – all of this carrying over into what has been built to stand here now.',
+            address:'45 Spring St, Melbourne VIC 3004',
             location: {
                 center: [144.9739482,-37.81419757],
                 zoom: 18.91,
@@ -295,36 +301,6 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        },
-
-        {
-            id: 'sumup',
-            alignment: 'full',
-            hidden: false,
-            title: 'SUMUP',
-            image: '',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [144.9739482,-37.81419757],
-                zoom: 18.91,
-                pitch: 46.50,
-                bearing: -73.20
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-              //  {
-              //      layer: 'food',
-              //      opacity: 1
-               // }
-            ],
-            onChapterExit: [
-               // {
-               //     layer: 'food',
-               //     opacity: 0
-               // }                
-            ]
         }
     ]
 };
